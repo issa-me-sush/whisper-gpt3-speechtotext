d8fbb3258c98f2610fc8f98dd323fd793208e8b8
sushthecoda <coolsushaan@gmail.com>
2023-02-10 23:40:21 +0530

    package fixes

--- next.config.js
+++ next.config.js
@@ -1,34 +1,6 @@
-const path = require("path");
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.coffee$/,
-      use: [
-        {
-          loader: "coffee-loader",
-          options: {
-            transpile: {
-              presets: ["@babel/preset-env"],
-            },
-          },
-        },
-      ],
-      include: [path.resolve(__dirname, "node_modules/flac.js")],
-    });
-    config.module.rules.push({
-      test: /\.js$/,
-      use: [
-        {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      ],
-      include: [path.resolve(__dirname, "node_modules/flac.js")],
-    });
-    return config;
-  },
-};
+}
+
+module.exports = nextConfig
